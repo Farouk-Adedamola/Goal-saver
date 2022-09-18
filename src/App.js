fe74483dcc9c8a07fe8b409a6e7c43185f1e9cbb
unknown <faroukadedamola1@gmail.com>
2022-09-18 16:03:09 +0100

    useState modified in src/App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import GoalList from "./GoalList";
 
 function App() {
-
   return (
     <>
       <GoalList />
